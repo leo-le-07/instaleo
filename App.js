e37c1ceea9c64db0ee5a8ef0318de81c28cef2bc
Leo <coder.leo.le@gmail.com>
2019-07-18 16:34:16 +0700

    Add Icon for tab bar

--- App.js
+++ App.js
@@ -1,16 +1,44 @@
-import React, { Fragment } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 import HomeScreen from "./src/components/HomeScreen";
 import FavouriteScreen from "./src/components/FavouriteScreen";
 
-import { OFFSET } from "./src/utils/constants";
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let iconName = "question";
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Favourite: FavouriteScreen
-});
+  if (routeName === "Home") {
+    iconName = "home";
+  } else if (routeName === "Favourite") {
+    iconName = "heart";
+  }
+
+  return <Icon name={iconName} light size={25} color={tintColor} />;
+};
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Favourite: FavouriteScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor)
+    })
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
+  }
+);
 
 const AppContainer = createAppContainer(TabNavigator);
 
