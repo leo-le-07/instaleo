fa4261ea55dc4de868eb23031f275d9b3d4a3eef
Leo <coder.leo.le@gmail.com>
2019-07-18 15:54:06 +0700

    Add react-navigation

---
+++ src/components/FavouriteScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const FavouriteScreen = () => (
+  <View style={styles.container}>
+    <View style={styles.messageContainer}>
+      <Text>Favourite</Text>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  messageContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default FavouriteScreen;
